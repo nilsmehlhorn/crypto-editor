e9f0ed8921fc2e05eb250bab519d26a60ceeffde
nils <nils.mehlhorn@arcor.de>
2016-05-09 17:59:50 +0200

    Add Bouncy Castle Dependency and provider registration

--- src/main/java/org/hsd/cryptoeditor/App.java
+++ src/main/java/org/hsd/cryptoeditor/App.java
@@ -1,22 +1,32 @@
-package org.hsd.cryptoeditor; /**
+package org.hsd.cryptoeditor;
+/**
  * Created by nils on 18.04.2016.
  */
-import javafx.application.*;
+
+import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.hsd.cryptoeditor.service.DialogService;
 
-public class App extends Application{
+import java.security.Security;
+
+public class App extends Application {
 
     public static void main(String... args) {
+        // register provider for bc
+        if(Security.getProvider("BC") == null) {
+            Security.addProvider(new BouncyCastleProvider());
+        }
+        // launch jfx
         launch(App.class);
     }
 
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/main_view.fxml"));
-        Scene scene = new Scene(root, 1280, 720);
+        Scene scene = new Scene(root, 600, 400);
         primaryStage.setTitle("Crypto Editor");
         primaryStage.setScene(scene);
         primaryStage.show();
