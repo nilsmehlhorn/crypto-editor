0232b9aa9b29fe609dd62c5201b65a5056a21570
nils <nils.mehlhorn@arcor.de>
2016-05-10 16:13:56 +0200

    Add placeholder test

---
+++ src/test/java/org/hsd/cryptoeditor/concurrency/SaveServiceTest.java
@@ -0,0 +1,22 @@
+package org.hsd.cryptoeditor.concurrency;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by nils on 5/10/16.
+ */
+public class SaveServiceTest {
+    @Before
+    public void setUp() throws Exception {
+
+
+    }
+
+    @Test
+    public void testSaving() throws Exception {
+
+    }
+}
