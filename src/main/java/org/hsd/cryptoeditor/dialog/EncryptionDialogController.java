84f3a3ca439da6e26d59c06862df0fdd10cd54db
Nils Mehlhorn <nils.mehlhorn@arcor.de>
2016-05-29 23:38:29 +0200

    Refactor encryption dialog

--- src/main/java/org/hsd/cryptoeditor/dialog/EncryptionDialogController.java
+++ src/main/java/org/hsd/cryptoeditor/dialog/EncryptionDialogController.java
@@ -1,22 +1,18 @@
 package org.hsd.cryptoeditor.dialog;
 
 import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.geometry.Insets;
-import javafx.scene.Node;
 import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
-import javafx.scene.text.Text;
 import org.hsd.cryptoeditor.crypto.CryptoService;
 import org.hsd.cryptoeditor.crypto.encryption.Encryption;
 import org.hsd.cryptoeditor.crypto.encryption.EncryptionMode;
 import org.hsd.cryptoeditor.crypto.encryption.EncryptionPadding;
 import org.hsd.cryptoeditor.crypto.encryption.EncryptionType;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -28,48 +24,38 @@ public class EncryptionDialogController {
     private BorderPane borderPane;
     @FXML
     private ListView<EncryptionType> encryptionTypeList;
-    private Map<EncryptionType, Encryption> mapping;
+    @FXML
+    private GridPane settingsPane;
+    @FXML
+    private ComboBox<EncryptionMode> blockModeDropdown;
+    @FXML
+    private ComboBox<EncryptionPadding> paddingDropdown;
+
+    private Map<EncryptionType,Encryption> mapping = new HashMap<>();
+
+    private Encryption selected;
 
     @FXML
     public void initialize() {
-        // load encryption types
-        mapping = new HashMap<>();
         for(EncryptionType type : EncryptionType.values()) {
             mapping.put(type, CryptoService.getInstance().getEncryption(type));
         }
-        ObservableList<EncryptionType> types = FXCollections.observableArrayList(mapping.keySet());
-        encryptionTypeList.setItems(types);
-
-        encryptionTypeList.getSelectionModel().selectedItemProperty().addListener(observable -> {
-            setEncryptionView(encryptionTypeList.getSelectionModel().getSelectedItem());
+        encryptionTypeList.setItems(FXCollections.observableList(Arrays.asList(EncryptionType.values())));
+        blockModeDropdown.setItems(FXCollections.observableList(Arrays.asList(EncryptionMode.values())));
+        encryptionTypeList.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            switchToType(newValue);
+        });
+        encryptionTypeList.getSelectionModel().select(EncryptionType.NONE);
+        blockModeDropdown.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            switchToMode(newValue);
+        });
+        paddingDropdown.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            selected.setPadding(newValue);
         });
     }
 
-    private void setEncryptionView(EncryptionType type) {
-        Node n = null;
-        if(type == EncryptionType.NONE) {
-            n = new Text("Select an encryption from the list");
-        } else {
-            Encryption encryption = mapping.get(type);
-            GridPane settingsView = new GridPane();
-            settingsView.setPadding(new Insets(25, 10, 0, 10));
-            settingsView.setHgap(20);
-            settingsView.add(new Label("Block Cipher Mode"), 0, 0);
-            ObservableList<EncryptionMode> modes = FXCollections.observableArrayList(EncryptionMode.values());
-            ComboBox<EncryptionMode> modeBox = new ComboBox<>(modes);
-            modeBox.setValue(encryption.getMode());
-            modeBox.setOnAction(event -> encryption.setMode(modeBox.getValue()));
-            settingsView.add(modeBox, 1, 0);
-
-            settingsView.add(new Label("Padding Method"), 0, 1);
-            ObservableList<EncryptionPadding> paddings = FXCollections.observableArrayList(encryption.getPossiblePaddings());
-            ComboBox<EncryptionPadding> padBox = new ComboBox<>(paddings);
-            padBox.setValue(encryption.getPadding());
-            padBox.setOnAction(event -> encryption.setPadding(padBox.getValue()));
-            settingsView.add(padBox, 1, 1);
-            n = settingsView;
-        }
-        borderPane.setCenter(n);
+    public Encryption getEncryption() {
+        return this.selected;
     }
 
     public void setEncryption(Encryption encryption) {
@@ -77,8 +63,29 @@ public class EncryptionDialogController {
         encryptionTypeList.getSelectionModel().select(encryption.getType());
     }
 
-    public Encryption getEncryption() {
-        return mapping.get(encryptionTypeList.getSelectionModel().getSelectedItem());
+    private void switchToMode(EncryptionMode mode) {
+        selected.setMode(mode);
+        if(mode.isStreamMode()) {
+            paddingDropdown.setDisable(true);
+        } else {
+            paddingDropdown.setDisable(false);
+            paddingDropdown.setItems(FXCollections.observableList(selected.getPossiblePaddings()));
+        }
+    }
+
+    private void switchToType(EncryptionType type) {
+        selected = mapping.get(type);
+        if(type.isStreamType() || type.equals(EncryptionType.NONE)) {
+            blockModeDropdown.setDisable(true);
+            paddingDropdown.setDisable(true);
+        } else {
+            blockModeDropdown.setDisable(false);
+            paddingDropdown.setDisable(false);
+            // update possible modes&paddings
+            blockModeDropdown.getSelectionModel().select(selected.getMode());
+            paddingDropdown.setItems(FXCollections.observableList(selected.getPossiblePaddings()));
+            paddingDropdown.getSelectionModel().select(selected.getPadding());
+        }
     }
 }
 
