bc9c862226679213cdcb3894e63c4bfa7f8b15a1
Nils Mehlhorn <nils.mehlhorn@arcor.de>
2016-06-01 18:43:40 +0200

    Add some example documentation

--- src/main/java/org/hsd/cryptoeditor/App.java
+++ src/main/java/org/hsd/cryptoeditor/App.java
@@ -11,20 +11,32 @@ import javafx.stage.Stage;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.hsd.cryptoeditor.dialog.DialogService;
 
+import java.io.IOException;
 import java.security.Security;
 
 public class App extends Application {
 
+    /**
+     * Default starting point for the application.
+     * It will initialize an appropriate environment and start the JavaFX application.
+     *
+     * @param args execution parameters
+     */
     public static void main(String... args) {
         // register provider for bc
-        if(Security.getProvider("BC") == null) {
+        if (Security.getProvider("BC") == null) {
             Security.addProvider(new BouncyCastleProvider());
         }
         // launch jfx
         launch(App.class);
     }
 
-    public void start(Stage primaryStage) throws Exception {
+    /**
+     * Starting method to be called by JavaFX framework
+     * @param primaryStage base stage for ui application
+     * @throws IOException when resource for main view can not be loaded
+     */
+    public void start(Stage primaryStage) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("/main_view.fxml"));
         Scene scene = new Scene(root, 600, 400);
         primaryStage.setTitle("Crypto Editor");
